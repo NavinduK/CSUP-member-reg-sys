08d1e28b29e2b089a6f5ff77b86610588e242293
viduranga-bit <amithrathnayaka34@gmail.com>
2020-12-01 00:47:55 +0530

    created register UI and rout two pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Main from './Component/Main/main';
-
+import { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Main from "./Component/Main/main";
+import Register from "./Component/Register/register";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-        <Main/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
