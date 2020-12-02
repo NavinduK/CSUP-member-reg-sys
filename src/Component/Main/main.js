759be0320fd53471900458b0df1dcb885082d621
navinduk <navindu.nk@gmail.com>
2020-12-02 21:54:01 +0530

    Register Num validation process of server and client

--- src/Component/Main/main.js
+++ src/Component/Main/main.js
@@ -1,24 +1,66 @@
-import React from "react";
-import styles from './main.module.scss';
+import React,{useState} from "react";
+import styles from './Main.module.scss';
+import { useFetch } from 'use-http';
+import { useHistory } from "react-router-dom";
+import { useForm} from "react-hook-form";
 
-const main = () => {
-  return (
-    <div className="container text-center" >
-      <div className={`${styles.wrapCenter} row justify-content-center align-content-center`} >
-        <div className="col ">
-          <h3>Login</h3>
-          <form>
-            <input
-              type="text"
-              className={`${styles.inputCenter} form-control`}
-              placeholder="Enter Your Registration Number"
-            />
-            <input className="btn btn-primary" type="submit" value="Submit" />
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+const Main = () => {
+
+	const { post, response } = useFetch("http://localhost:3001/csup");
+
+	const history = useHistory();
+
+	const [regNo, setRegNo] = useState()
+
+
+
+
+	let value = {
+        regNo: ''
+    }
+
+	const { handleSubmit, register, errors } = useForm({
+        mode: "onBlur",
+        defaultValues: value
+    });
+
+	const submitRegNo = async (event) => {
+		console.log(event)
+		await
+		post('/login/admin', event).then(result => {
+			if (response.ok) {
+				history.push('/adminlogin');
+			} else {
+				history.push('/register');
+			}
+
+		}).catch(err => {
+			console.log('error', err);
+		})
+	};
+	
+	return (
+		<div className="container text-center" >
+		  <div className={`${styles.wrapCenter} row justify-content-center align-content-center`} >
+			<div className="col ">
+			  <h3>Login</h3>
+			  <form onSubmit={handleSubmit(submitRegNo)}>
+				<input
+				  type="text"
+				  name="regNo"
+				  className={`${styles.inputCenter} form-control`}
+				  placeholder="Enter Your Registration Number"
+				  aria-invalid={errors.email ? "true" : "false"}
+				  ref={register({
+					required: "email is required"
+				  })}
+				/>
+				<input className="btn btn-primary" type="submit" value="Submit" />
+			  </form>
+			</div>
+		  </div>
+		</div>
+	  );
 };
 
-export default main;
+export default Main;
